3ce5f1f7398d87b2dfe07c3006149cad4e7b294d
William Chang <mr.williamchang@gmail.com>
2016-01-12 11:07:08 -0800

    Move pretty print into grids module

--- problem502/main.py
+++ problem502/main.py
@@ -1,7 +1,6 @@
 import blocks
 import grids
-import grid_utils
 
 grid = grids.Grid(4, 3)
 assert grid.maybe_add_block(blocks.Block(0, 3), 0)
-grid_utils.pretty_print(grid)
+grids.pretty_print(grid)
